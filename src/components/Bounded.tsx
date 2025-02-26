994f7c7bb221e2216869d114eafb1bdbd75d7ff2
Alex Kadai <alexandrkadai@gmail.com>
2025-02-26 23:23:12 +0200

    Refactor

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -1,5 +1,5 @@
-import { CSSProperties, ElementType, ReactNode } from "react";
-import clsx from "clsx";
+import { CSSProperties, ElementType, ReactNode } from 'react';
+import clsx from 'clsx';
 
 type BoundedProps = {
   as?: ElementType;
@@ -8,20 +8,11 @@ type BoundedProps = {
   children: ReactNode;
 };
 
-export function Bounded({
-  as: Comp = "section",
-  className,
-  children,
-  ...restProps
-}: BoundedProps) {
+export function Bounded({ as: Comp = 'section', className, children, ...restProps }: BoundedProps) {
   return (
     <Comp
-      className={clsx(
-        "px-6 ~py-10/16 [.header+&]:pt-44 [.header+&]:md:pt-32",
-        className,
-      )}
-      {...restProps}
-    >
+      className={clsx('px-6 ~py-10/16 [.header+&]:pt-44 [.header+&]:md:pt-32', className)}
+      {...restProps}>
       <div className="mx-auto w-full max-w-6xl">{children}</div>
     </Comp>
   );
