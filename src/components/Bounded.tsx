1085fa02ec8fadd21db1843c3a9b189f9cbd12de
Alex Kadai <alexandrkadai@gmail.com>
2025-03-04 23:50:07 +0200

    Prettier config

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -8,11 +8,20 @@ type BoundedProps = {
   children: ReactNode;
 };
 
-export function Bounded({ as: Comp = 'section', className, children, ...restProps }: BoundedProps) {
+export function Bounded({
+  as: Comp = 'section',
+  className,
+  children,
+  ...restProps
+}: BoundedProps) {
   return (
     <Comp
-      className={clsx('px-6 ~py-10/16 [.header+&]:pt-44 [.header+&]:md:pt-32', className)}
-      {...restProps}>
+      className={clsx(
+        'px-6 ~py-10/16 [.header+&]:pt-44 [.header+&]:md:pt-32',
+        className
+      )}
+      {...restProps}
+    >
       <div className="mx-auto w-full max-w-6xl">{children}</div>
     </Comp>
   );
